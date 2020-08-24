27469ea24994d5cd19ffd6cd30b160d3f8a6c351
parsa mahmoudy <43126637+parsa011@users.noreply.github.com>
2020-08-24 11:56:52 +0430

    started for creating entities

---
+++ EzGame.Domain/Entities/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EzGame.Domain.Core.Services;
+using EzGame.Domain.Core.Services.Entities;
+
+namespace EzGame.Domain.Entities
+{
+    public class Platform : BaseEntity<string>
+    {
+        public Platform()
+        {
+            CreatedTime = DateTime.Now;
+            Id = IdGenerator.NewGuid();
+            LastModifiedTime = DateTime.Now;
+        }
+
+        public string Title { get; set; }
+        public string Logo { get; set; }
+    }
+}
