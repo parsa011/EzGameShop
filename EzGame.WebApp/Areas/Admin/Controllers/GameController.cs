0e7f0183cfb33263f934eccafcd6ec86e7c2cf04
Mostafa-ma2 <64909038+Mostafa-ma2@users.noreply.github.com>
2020-08-25 17:12:11 +0430

    Add Game Controller :D

---
+++ EzGame.WebApp/Areas/Admin/Controllers/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EzGame.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+
+namespace EzGame.WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GameController : Controller
+    {
+        private readonly IUnitOfWork _db;
+        private readonly IToastNotification _notification;
+        public GameController(IUnitOfWork db, IToastNotification notification)
+        {
+            _db = db;
+            _notification = notification;
+        }
+
+        //Get
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddGame()
+        {
+            return View();
+        }
+    }
+}
