6a8dbf323ab30f0ccafc7b2d539a538dec256662
parsa mahmoudy <43126637+parsa011@users.noreply.github.com>
2020-08-25 14:51:26 +0430

    edit mostafa codes

--- EzGame.WebApp/Areas/Admin/Controllers/PlatformController.cs
+++ EzGame.WebApp/Areas/Admin/Controllers/PlatformController.cs
@@ -20,32 +20,35 @@ namespace EzGame.WebApp.Areas.Admin.Controllers
             _db = db;
             _notification = notification;
         }
+        
         public async Task<ActionResult> Index()
         {
-            var getAll = await _db.PlatformRepository.GetAllAsync();
+            var getAll = await _db.PlatformRepository.GetAllAsync(a=>!a.IsDeleted);
             return View(getAll);
         }
-        public async Task<ActionResult> AddPlatform(string Title,string isDeleted,string Logo)
+        
+        public async Task<ActionResult> AddPlatform(string title,string isDeleted,string logo)
         {
-            if (!string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(isDeleted) && !string.IsNullOrEmpty(Logo))
+            if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(isDeleted) && !string.IsNullOrEmpty(logo))
             {
                 var platform = new Platform()
                 {
-                    Title = Title,
-                    Logo = Logo,
-                    IsDeleted = isDeleted == "حذف شده" ? true : false,
+                    Title = title,
+                    Logo = logo,
+                    IsDeleted = isDeleted == "حذف شده",
                     CreatedTime=DateTime.Now,
                     LastModifiedTime=DateTime.Now
                 };
                 await _db.PlatformRepository.InsertAsync(platform);
                  _db.SaveChange();
-                _notification.AddSuccessToastMessage($"پلتفرم {Title} با موفقیت ساخته شد.");
+                _notification.AddSuccessToastMessage($"پلتفرم {title} با موفقیت ساخته شد.");
                 return Json(platform);
             }
-                _notification.AddErrorToastMessage("نام و وعضیت نمی تواند خالی باشد");
+            _notification.AddErrorToastMessage("نام و وضعیت نمی تواند خالی باشد");
             return Json(null);
         }
-        public async Task<ActionResult> GetPlatformByID(string id)
+        
+        public async Task<ActionResult> GetPlatformById(string id)
         {
             if (!string.IsNullOrEmpty(id))
             {
@@ -55,30 +58,33 @@ namespace EzGame.WebApp.Areas.Admin.Controllers
             _notification.AddErrorToastMessage("دوباره امتحان کنید");
             return Json(null);
         }
-        public ActionResult UpdateGroup(string ID, string Title, string isDeleted,string logo)
+        
+        public ActionResult UpdatePlatform(string id, string title, string isDeleted,string logo)
         {
-            if (!string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(ID) && !string.IsNullOrEmpty(isDeleted) && !string.IsNullOrEmpty(logo))
+            if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(id) && !string.IsNullOrEmpty(isDeleted) && !string.IsNullOrEmpty(logo))
             {
-                var platform = _db.PlatformRepository.GetById(ID);
-                platform.IsDeleted = isDeleted == "حذف شده" ? true : false;
-                platform.Title = Title;
+                var platform = _db.PlatformRepository.GetById(id);
+                platform.IsDeleted = isDeleted == "حذف شده";
+                platform.Title = title;
                 platform.Logo = logo;
                 platform.LastModifiedTime = DateTime.Now;
                 _db.PlatformRepository.Update(platform);
                 _db.SaveChange();
-                _notification.AddSuccessToastMessage($"پلتفرم {Title} با موفقیت ویرایش شد.");
+                _notification.AddSuccessToastMessage($"پلتفرم {title} با موفقیت ویرایش شد.");
                 return Json(platform);
 
             }
             _notification.AddErrorToastMessage("مقادیر نمی توانند خالی باشند");
             return Json(null);
         }
-        public ActionResult DeleteGroup(string id)
+        
+        public ActionResult DeletePlatform(string id)
         {
             if (!string.IsNullOrEmpty(id))
             {
                 var platform = _db.PlatformRepository.GetById(id);
-                _db.PlatformRepository.Delete(platform);
+                platform.IsDeleted = true;
+                _db.PlatformRepository.Update(platform);
                 _db.SaveChange();
                 _notification.AddSuccessToastMessage($"پلتفرم {platform.Title} با موفقیت حذف شد.");
                 return Json(platform);
