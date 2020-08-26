c075fc11b4dff0d83e2157d61377f00946200d2e
Mostafa-ma2 <64909038+Mostafa-ma2@users.noreply.github.com>
2020-08-26 12:03:45 +0430

    Edit Game Index

---
+++ EzGame.Common/ViewModel/Games/GameIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EzGame.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EzGame.Common.ViewModel.Games
+{
+    public class GameIndexViewModel
+    {
+        public IEnumerable<Game> Games { get; set; }
+        public IEnumerable<GameGenre> GameGenres { get; set; }
+        public IEnumerable<GamePlatform> gamePlatforms { get; set; }
+    }
+}
