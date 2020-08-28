bd0bed4b0ea0a78ea18bd85234f33df6f6a8f9fa
Mostafa-ma2 <64909038+Mostafa-ma2@users.noreply.github.com>
2020-08-28 14:25:50 +0430

    Add AccountController and viewmodel

---
+++ EzGame.Common/ViewModel/Account/AccountLoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EzGame.Common.ViewModel.Account
+{
+   public class AccountLoginViewModel
+    {
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [Display(Name = "ایمیل")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [Display(Name = "رمز عبوز")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+}
