d89bc19ddf52107a8ecc77f81be07c7a038db34e
Mostafa-ma2 <64909038+Mostafa-ma2@users.noreply.github.com>
2020-08-28 19:24:47 +0430

    Add AddGameEdition

---
+++ EzGame.Data/Migrations/20200828144858_init.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EzGame.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GameId",
+                table: "GameEditions",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameEditions_GameId",
+                table: "GameEditions",
+                column: "GameId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameEditions_Games_GameId",
+                table: "GameEditions",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GameEditions_Games_GameId",
+                table: "GameEditions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GameEditions_GameId",
+                table: "GameEditions");
+
+            migrationBuilder.DropColumn(
+                name: "GameId",
+                table: "GameEditions");
+        }
+    }
+}
